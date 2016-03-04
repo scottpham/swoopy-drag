bd4843d944544d3b00b9663117d0f69b5b9a0dd0
Adam Pearce <apearce19@bloomberg.com>
2016-03-04 18:23:30 -0500

    rough path string

---
+++ script.js
@@ -0,0 +1,52 @@
+var annotations = [
+  {
+    x: 4.4,
+    y: 5.7,
+    path: 'M -5 -5 L -20 -20',
+    text: 'Green Flower',
+    textOffset: [-60, -30]
+  }
+]
+
+var annotationText = d3.select('body').append('pre')
+    .style('margin-top', '300px')
+
+
+
+d3.tsv('data.tsv', function(data){
+  c = d3.conventions({parentSel: d3.select('#graph')})
+
+  c.x.domain(d3.extent(data, ƒ('sepalWidth')) ).nice()
+  c.y.domain(d3.extent(data, ƒ('sepalLength'))).nice()
+  c.drawAxis()
+
+  c.svg.dataAppend(data, 'circle')
+      .attr('cx', ƒ('sepalWidth', c.x))
+      .attr('cy', ƒ('sepalLength',c.y))
+      .attr('fill', ƒ('species', c.color))
+      .attr({r: 5, stroke: '#000'})
+      .call(d3.attachTooltip)
+
+  var legend = c.svg.dataAppend(c.color.domain(), 'g.legend')
+      .translate(function(d, i){ return [0, i*20] })
+
+  legend.append('rect')
+      .attr({x: c.width - 18, width: 18, height: 18})
+      .style('fill', c.color)
+
+  legend.append('text')
+      .attr({x: c.width - 24, y: 9, dy: '.33em', 'text-anchor': 'end'})
+      .text(ƒ())
+
+
+  swoopy = d3.swoopyDrag()
+      .annotations(annotations)
+      .x(c.x)
+      .y(c.y)
+
+  c.svg.append('g').call(swoopy)
+
+})
+
+
+
---
+++ swoopy-drag.js
@@ -0,0 +1,116 @@
+d3.swoopyDrag = function(){
+  var x = d3.scale.linear()
+  var y = d3.scale.linear()
+
+  var annotations = []
+  var annotationSel
+
+  var textDrag = d3.behavior.drag()
+      .on('drag', function(d){
+        var pos = d3.mouse(this.parentNode)
+        var x = pos[0] 
+        var y = pos[1] 
+        var offset = [x, y].map(Math.round)
+
+        console.log("sdfsdf")
+
+        d3.select(this).translate(offset)
+        d3.select(this).datum().textOffset = offset
+
+        update()
+      })
+      .origin(function(d){
+        return d.textOffset
+        return {x: d.textOffset[0], y: d.textOffset[1]}
+      })
+
+
+  var circleDrag = d3.behavior.drag()
+      .on('drag', function(d){
+        var pos = d3.mouse(this.parentNode)
+        var x = pos[0] 
+        var y = pos[1] 
+        var offset = [x, y].map(Math.round)
+
+        var parentSel = d3.select(this.parentNode)
+
+        var circles = parentSel.selectAll('circle').data()
+        var path = ''
+        circles.forEach(function(d){
+          path = path + ' ' + d.type + ' ' + d.pos
+        })
+
+        console.log(path)
+
+        parentSel.select('path').attr('d', path).datum().path = path
+        d3.select(this).translate(offset).datum().pos = offset
+
+        update()
+      })
+      // .origin(function(d){
+      //   return d.textOffset
+      //   return {x: d.textOffset[0], y: d.textOffset[1]}
+      // })
+
+
+  var rv = function(sel){
+    annotationSel = sel.dataAppend(annotations, 'g.annotation')
+        .translate(function(d){ return [x(d.x), y(d.y)] })
+    
+    annotationSel.append('text')
+        .translate(ƒ('textOffset'))
+        .text(ƒ('text'))
+        .call(textDrag)
+
+    annotationSel.append('path')
+        .attr('d', ƒ('path'))
+
+    annotationSel.dataAppend(function(d){
+      var points = []
+
+      var i = 1
+      var type = 'M'
+      for (var j = 1; j < d.path.length; j++){
+        var curChar = d.path[j]
+        if (curChar == 'L' || curChar == 'Q'){
+          points.push({pos: d.path.substr(i, j - 1), type: type, parentData: d})
+          type = curChar
+          i = j + 1
+        }
+      }
+
+      points.push({pos: d.path.substr(i, j - 1), type: type, parentData: d})
+
+      console.log(points)
+      return points
+    }, 'circle')
+        .attr('r', 3)
+        .translate(ƒ('pos'))
+        .call(circleDrag)
+
+    update()
+  }
+
+
+  function update(){
+    annotationText.text(JSON.stringify(annotations, null, 2))
+  }
+
+  rv.annotations = function(_x){
+    if (!_x) return annotations
+    annotations = _x
+    return rv
+  }
+  rv.x = function(_x){
+    if (!_x) return x
+    x = _x
+    return rv
+  }
+  rv.y = function(_x){
+    if (!_x) return y
+    y = _x
+    return rv
+  }
+
+  return rv
+}
